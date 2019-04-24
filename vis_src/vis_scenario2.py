19a372ed5f58c83c7008a95aab0156f54bfe521f
azane <azane@nnu.edu>
2019-04-24 11:59:42 -0400

    works on abstracting drawable vehicle.

--- vis_src/vis_scenario2.py
+++ vis_src/vis_scenario2.py
@@ -3,26 +3,39 @@ import numpy as np
 
 from vispy import app, scene
 
+
+class Vehicle(object):
+    def __init__(self, vehicle, scene_):
+        super().__init__()
+        assert vehicle.shape == (4,2)
+        self.vehicle = vehicle
+        self.arrow = self.vehicle[[1, 2, 2]]
+        self.arrow[1, 1] += 0.5
+        self.scene = scene_
+
+        self._pa = scene.visuals.Polygon(pos=self.arrow, parent=self.scene, color='grey')
+        self._pv = scene.visuals.Polygon(pos=self.vehicle, parent=self.scene, color='white')
+
+    def draw(self, f):
+        self._pa.set_data(f(self.arrow))
+        self._pa.update()
+        self._pv.set_data(f(self.vehicle))
+        self._pv.update()
+
 vehicle = np.array([
     [0., 0.5],
     [2., 0.5],
     [2., -0.5],
     [0., -0.5]
 ])
-arrow = np.array([
-    [1.5, 0.5],
-    [2., 0.],
-    [1.5, -0.5]
-])
 
 canvas = scene.SceneCanvas(size=(500, 500), keys='interactive')
 
-scene.visuals.Polygon(pos=arrow*10+200, parent=canvas.scene, color='grey')
-scene.visuals.Polygon(pos=vehicle*10+200, parent=canvas.scene, color='white')
+v = Vehicle(vehicle, canvas.scene)
 
 
 def update(event):
-    pass
+    v.draw(lambda x: x * 50 + np.random.randint(100, 300))
 
 
 timer = app.Timer('auto', connect=update, start=True)
